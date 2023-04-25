dacb88dc8e7c92282b5c589eb8f059e71d920ae0
TonyWang <125868838+hmtonywang@users.noreply.github.com>
2023-04-25 14:31:45 +0800

    Listen on port 3000 by default

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports.env = process.env.NODE_ENV || 'development';
-module.exports.port = process.env.PORT || 80;
+module.exports.port = process.env.PORT || 3000;
 module.exports.proxyNumber = process.env.PROXY_NUMBER || 1;
 module.exports.version = process.env.VERSION || 'v1';
 module.exports.rateLimit = process.env.RATE_LIMIT !== 'false';
